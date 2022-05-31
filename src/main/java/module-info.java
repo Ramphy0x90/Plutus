8fcfd21879c156d37493d62ed859369aca49a46a
ramphy0x90 <ramphy11@hotmail.com>
2022-05-31 10:25:17 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module hhccco.plutus {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens hhccco.plutus to javafx.fxml;
+    exports hhccco.plutus;
+    exports hhccco.plutus.controllers;
+    opens hhccco.plutus.controllers to javafx.fxml;
+}
