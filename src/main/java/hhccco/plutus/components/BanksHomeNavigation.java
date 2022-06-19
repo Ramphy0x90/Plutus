46b95693220094e7a9e8345ba2a4f0b1b84131c3
ramphy0x90 <ramphy11@hotmail.com>
2022-06-19 21:54:59 +0200

    Banks home navigation

---
+++ src/main/java/hhccco/plutus/components/BanksHomeNavigation.java
@@ -0,0 +1,56 @@
+package hhccco.plutus.components;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+
+import java.util.HashMap;
+
+public class BanksHomeNavigation extends HBox {
+    static HashMap<String, Node> nodesObjects = new HashMap<>();
+
+    public BanksHomeNavigation() {
+        this.setId("banksHomeNavigation");
+        this.setSpacing(5);
+        this.setPadding(new Insets(10));
+
+        initNodes();
+        addChildren();
+    }
+
+    private void initNodes(){
+        nodesObjects.put("bankLabel", new Label("Banca"));
+        nodesObjects.put("bankPicker", new ComboBox<String>());
+        nodesObjects.put("accountNumber", new Label("Num. Conto"));
+        nodesObjects.put("accountNumberField", new TextField());
+        nodesObjects.put("accountType", new Label("Tipo Conto"));
+        nodesObjects.put("accountTypeField", new TextField());
+
+        ((ComboBox<?>) nodesObjects.get("bankPicker")).setPrefWidth(150);
+
+        ((TextField) nodesObjects.get("accountNumberField")).setPrefWidth(100);
+        ((TextField) nodesObjects.get("accountTypeField")).setPrefWidth(100);
+
+        ((TextField) nodesObjects.get("accountNumberField")).setEditable(false);
+        ((TextField) nodesObjects.get("accountTypeField")).setEditable(false);
+    }
+
+    private void addChildren() {
+        Separator separator1 = new Separator(Orientation.VERTICAL);
+        Separator separator2 = new Separator(Orientation.VERTICAL);
+
+        separator1.getStyleClass().add("separator");
+        separator2.getStyleClass().add("separator");
+
+        this.getChildren().add(nodesObjects.get("bankLabel"));
+        this.getChildren().add(nodesObjects.get("bankPicker"));
+        this.getChildren().add(separator1);
+        this.getChildren().add(nodesObjects.get("accountNumber"));
+        this.getChildren().add(nodesObjects.get("accountNumberField"));
+        this.getChildren().add(separator2);
+        this.getChildren().add(nodesObjects.get("accountType"));
+        this.getChildren().add(nodesObjects.get("accountTypeField"));
+    }
+}
--- src/main/java/hhccco/plutus/components/Body.java
+++ src/main/java/hhccco/plutus/components/Body.java
@@ -10,13 +10,12 @@ import javafx.scene.layout.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 
 public class Body extends GridPane {
     public static final DBconnection dbConn = new DBconnection();
     static HashMap<String, Node> nodesObjects = new HashMap<>();
+    private final GridPane centerColumnLayout = new GridPane();
 
     public Body() {
         this.setId("body");
@@ -30,28 +29,44 @@ public class Body extends GridPane {
             System.err.println("SQLite error: \n\t" + e);
         }
 
+        centerColumnLayout.add(nodesObjects.get("banksNavigation"), 0, 0);
+        centerColumnLayout.add(nodesObjects.get("tableData"), 0, 1);
+
         this.add(nodesObjects.get("leftMenu"), 0, 0);
-        this.add(nodesObjects.get("tableData"), 1, 0);
+        this.add(centerColumnLayout, 1, 0);
     }
 
     private void setStruct() {
+        RowConstraints mainRow = new RowConstraints();
+        mainRow.setVgrow(Priority.ALWAYS);
+
         ColumnConstraints leftColumn = new ColumnConstraints();
         leftColumn.setPrefWidth(200);
 
         ColumnConstraints centerColumn = new ColumnConstraints();
         centerColumn.setHgrow(Priority.ALWAYS);
 
-        RowConstraints mainRow = new RowConstraints();
-        mainRow.setVgrow(Priority.ALWAYS);
-
         this.getRowConstraints().add(mainRow);
         this.getColumnConstraints().add(0, leftColumn);
         this.getColumnConstraints().add(1, centerColumn);
+
+        ColumnConstraints mainColumn = new ColumnConstraints();
+        mainColumn.setHgrow(Priority.ALWAYS);
+
+        RowConstraints rowTop = new RowConstraints();
+
+        RowConstraints rowBottom = new RowConstraints();
+        rowBottom.setVgrow(Priority.ALWAYS);
+
+        centerColumnLayout.getColumnConstraints().add(mainColumn);
+        centerColumnLayout.getRowConstraints().add(rowTop);
+        centerColumnLayout.getRowConstraints().add(rowBottom);
     }
 
     private void initNodes() {
-        nodesObjects.put("tableData", new TableData().getTable());
         nodesObjects.put("leftMenu", new LeftMenu());
+        nodesObjects.put("banksNavigation", new BanksHomeNavigation());
+        nodesObjects.put("tableData", new TableData().getTable());
     }
 
     public static void populateDataTable(String ...date) throws SQLException {
@@ -62,6 +77,7 @@ public class Body extends GridPane {
 
         if(date.length == 0) {
             movementsDate = dbConn.getMovementsLastDate();
+
             ((DatePicker) LeftMenu.nodesObjects.get("datePicker")).setValue(LocalDate.parse(movementsDate));
         } else {
             movementsDate = date[0];
